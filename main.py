999c3b5549cfbf4e4a0cbd9591787d25892e522f
Lucas Rios <lucasriosferreira.564@gmail.com>
2025-10-24 18:06:29 -0300

    continuacao do projeto

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def mostrar_menu():
     print("\n=== Menú Principal ===")
     print("1. Cadastrar paciente")
     print("2. Listar todos os pacientes")
-    print("3. Sair")
+    print("3. Consultar paciente")
+    print("4. Alterar dados de um paciente")
+    print("5. Sair")
 
 def main():
     pacientes = []
@@ -14,7 +16,9 @@ def main():
         if escolha == '1':
             nome = input("Nome do paciente: ")
             idade = input("Idade do paciente: ")
-            pacientes.append({'nome': nome, 'idade': idade})
+            cpf = input("CPF do paciente: ")
+            endereco = input("Endereço do paciente: ")
+            pacientes.append({'nome': nome, 'idade': idade, 'cpf': cpf, 'endereco': endereco})
             print("Paciente cadastrado")
         elif escolha == '2':
             if not pacientes:
@@ -23,7 +27,48 @@ def main():
                 print("\n=== Lista de Pacientes ===")
                 for paciente in pacientes:
                     print("Nome:", paciente['nome'], "- Idade:", paciente['idade'])
+
         elif escolha == '3':
+            busca = input("Digite o CPF ou nome do paciente: ")
+            encontrado = False
+            for paciente in pacientes:
+                if busca == paciente['cpf'] or busca == paciente['nome']: 
+                    encontrado = True
+                    print("Nome:", paciente['nome'], "- Idade:", paciente['idade'], "-CPF:", paciente['cpf'])
+
+            if not encontrado:
+                print("Paciente não encontrado")
+
+        elif escolha == '4':
+            alterar = input("Digite o CPF do paciente:")
+            encontrado = False
+            for paciente in pacientes:
+                if alterar == paciente['cpf']:
+                    encontrado = True
+                    novo_nome = input(f"Nome [{paciente['nome']}]: ")
+                    if novo_nome != "":
+                        paciente['nome'] = novo_nome
+
+                    nova_idade = input(f"Idade [{paciente['idade']}]: ")
+                    if nova_idade != "":
+                        paciente['idade'] = nova_idade
+
+                    novo_endereco = input(f"Endereço [{paciente['endereco']}]: ")
+                    if novo_endereco != "":
+                        paciente['endereco'] = novo_endereco
+
+                    novo_cpf = input(f"CPF [{paciente['cpf']}]: ")
+                    if novo_cpf != "":
+                        paciente['cpf'] = novo_cpf   
+
+                    print("Paciente atualizado!")
+                    print("Nome:", paciente['nome'], "- Idade:", paciente['idade'], "-CPF:", paciente['cpf'])
+        
+            if not encontrado:
+                print("Paciente não encontrado")
+                
+            
+        elif escolha == '5':
             print("Saindo do programa!")
             break
         else:
